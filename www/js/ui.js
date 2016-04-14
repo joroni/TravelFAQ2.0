36a53da2848876ff317ecafec71cbef05c57c9f8
joroni <jraymund.niconi@gmail.com>
2016-04-14 10:39:39 +0800

    index an d ui js

--- www/js/ui.js
+++ www/js/ui.js
@@ -210,7 +210,7 @@ appUI.initiateDataUpdate = function() {
 		appUI.blocked = false;	
 	}
 
-	var afterOpen = function(event, ui) {		
+	/* var afterOpen = function(event, ui) {		
 		appUI.blocked = true;
 		$("#updateText").html("Downloading data...");
 		remoteStore.getData( ajax, 
@@ -253,7 +253,7 @@ appUI.initiateDataUpdate = function() {
 		
 		$("#updateProgressDialog").off("popupafteropen", afterOpen);
 			
-	};
+	};*/
 
 	$("#updateProgressDialog").on("popupafteropen", afterOpen);
 	
@@ -262,7 +262,7 @@ appUI.initiateDataUpdate = function() {
 	$("#updateProgressDialog").popup("open");
 }
 
-appUI.downloadFlags = function(finishCallback) {
+/*appUI.downloadFlags = function(finishCallback) {
 	
 	var folder = config.localImageFolderPath;
 	var remotePath = config.remoteServerAddress + '/' + config.remoteDataFlagsFolder + '/';
@@ -334,11 +334,13 @@ appUI.downloadFlags = function(finishCallback) {
 	} else {
 		if (finishCallback) finishCallback();
 	}
-}
+}*/
 	
 appUI.closeMenu = function() {
 	$("#optionsPanel").panel("close");
 }
+
+
 
 appUI.populateCountriesAll = function() {
 	localStore.getData("SELECT *", null, null, function(localData) {			
@@ -356,7 +358,11 @@ appUI.populateCountriesAll = function() {
 				icon.style.width = "30px";
 				icon.style.height = "30px";
 				
-				var iconPath = config.fileSystemRootFolder + '/' + config.localImageFolderPath + '/' + code.toLowerCase() + ".png?" + Math.random();				
+				//var iconPath = config.fileSystemRootFolder + '/' + config.localImageFolderPath + '/' + code.toLowerCase() + ".png?" + Math.random();	
+				
+				
+			var imgPath = config.localImageFolderPath + '/' + countryCode.toLowerCase() + ".png?" + Math.random();
+						
 				if (!localFileExists(iconPath)) {
 					iconPath = config.defaultIconPath;
 				}
@@ -403,6 +409,22 @@ appUI.clearCountryFilter = function() {
 
 appUI.positionListFilter = function() {
 	if (!config.fixCountryFilter) return;
+	
+		$("#allCountries li.ui-li-divider").show();
+		$("#allCountries li").hide();
+		$("#listFilter").show();
+		$("#listFilter").append($(".ui-filterable").detach());
+		$("#allCountries").css("padding-top","0px")
+		
+		
+	
+	
+	
+}
+
+/*
+appUI.positionListFilter = function() {
+	if (!config.fixCountryFilter) return;
 	if (config.tabletMode && appUI.isLandscape()) {
 		$("#listFilterLS").show();
 		$("#listFilterLS").append($(".ui-filterable").detach());
@@ -412,7 +434,7 @@ appUI.positionListFilter = function() {
 		$("#listFilter").append($(".ui-filterable").detach());
 		$("#allCountries").css("padding-top","0px")
 	}
-}
+}*/
 
 appUI.populateCountryDetails = function(countryCode) {
 	localStore.getData("SELECT *", "code like ?", [countryCode], function(localData) {			
@@ -429,7 +451,9 @@ appUI.populateCountryDetails = function(countryCode) {
 			if (pf && pf=="Android") mapShowURI = "geo:0,0?q=" + config.mapCoords1Key;
 			if (pf && pf=="Android") mapDirURI = "https://maps.google.com/?q=" + config.mapCoords2Key + "+to+" + config.mapCoords1Key;
 
-			var imgPath = config.fileSystemRootFolder + '/' + config.localImageFolderPath + '/' + countryCode.toLowerCase() + ".png?" + Math.random();
+		//	var imgPath = config.fileSystemRootFolder + '/' + config.localImageFolderPath + '/' + countryCode.toLowerCase() + ".png?" + Math.random();
+			
+				var imgPath = config.fileSystemRootFolder + '/' + config.localImageFolderPath + '/' + countryCode.toLowerCase() + ".png?" + Math.random();
 			
 			if (!localFileExists(imgPath)) {				
 				imgPath = config.defaultIconPath;
